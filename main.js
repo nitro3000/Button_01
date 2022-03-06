abcedb41f691883baed772d03faf02116bfc9b44
Andrey <andreyfaigon@gmail.com>
2022-03-06 17:39:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// let clickMe = document.createElement("button");
+// clickMe.innerHTML = "Click Me";
+// document.body.appendChild(clickMe);
+// clickMe.onclick = function(){alert("Button is clicked")};
+
+
+function abc() {
+  //var btn2 = document.getElementById();
+  btn1.onclick=function(){alert("ok")}
+}
+
+// function aaa (){
+// btn2.onclick=function(){ alert("oooo")}}
